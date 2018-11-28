f569e13be967a60c548a4d122d2b717d6a1e4d1a
Małgorzata Szlęzak <ameba01@poczta.fm>
2018-11-28 10:28:48 +0100

    - email validator

--- src/app/email.validator.ts
+++ src/app/email.validator.ts
@@ -2,7 +2,7 @@ import { AbstractControl } from '@angular/forms';
 
 export function ValidateEmail(control: AbstractControl) {
   const email = control.value;
-  const emailRegexp = new RegExp('^[a-zA-Z0-9!#$%&\'*+\/=?^_`{|}~.-]+@[a-z0-9-]+(\.[a-z0-9-]+)*$');
+  const emailRegexp = new RegExp('^[a-zA-Z0-9!#$%&\'*+\\\\/=?^_`{|}~-]+(?:\\.[a-zA-Z0-9!#$%&\'*+\\\\/=?^_`{|}~-]+)*@(?:[a-zA-Z0-9](?:[a-zA-Z0-9-]*[a-zA-Z0-9])?\\.)+[a-zA-Z0-9](?:[a-zA-Z0-9-]*[a-zA-Z0-9])?$');
 
   return !emailRegexp.test(email) ? { email: true } : null;
 }
